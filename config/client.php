db325aac7560c961f1d7016f599eb5e4daa71c9c
Marcel Stadelmann <m@marceli.to>
2023-09-28 10:40:18 +0200

    wip

--- config/client.php
+++ config/client.php
@@ -1,4 +1,5 @@
 <?php
 return [
   'base_url' => env('APP_URL'),
+  'meta_description' => 'Uto Legal Rechtsanwälte bieten rechtliche und strategische Beratung im Bereich Urheberrecht, Markenrecht, Musik, Film, Kunst, Medien, Kommunikation'
 ];
